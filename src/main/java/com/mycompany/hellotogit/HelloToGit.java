26959b00d93d00ffc157b735e4d02537da7ed3c7
Administrator <Administrator@WIN-NIQISOQUKS3>
2023-09-29 18:25:21 +0800

    initial commit

---
+++ src/main/java/com/mycompany/hellotogit/HelloToGit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hellotogit;
+
+/**
+ *
+ * @author Administrator
+ */
+public class HelloToGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
